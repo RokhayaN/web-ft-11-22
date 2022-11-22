02a734a04112c604c7e2bae588e4a3850757d124
RokhayaN <55556810+RokhayaN@users.noreply.github.com>
2022-11-22 14:56:54 -0600

    Create assignment.py
    adding the assignement to github

---
+++ week2/day2/assignment.py
@@ -0,0 +1,82 @@
+# 1. Write a function called "nameFunction" have it print out your name. invoke the function
+
+def  nameFunction():
+    print("My name is Rokhaya")
+
+
+#2. Define variable named Joe that is the string Joe
+
+Joe = "Joe"
+#3 Reinitialize Joe so that it is now the integer 4
+Joe = 4
+
+#4 Change Joe into a float
+Joe = 4.7
+
+#5. Create a list named students that includes 5 people from this class
+students = ["rokhaya","vinny","jessika","mike","ethan"]
+
+    #5a print the first item from the list(bonus for more than one way)
+print(students[0])
+
+    #5b print the last item from the list(bonus for more than one way)
+print(students[-1])
+    #5c How do I get the third item from the list?
+print(students[2])
+    #5d print the entire list not in brackets [student1, student2, etc...]
+for student in students:
+    print(student)
+    #5e add "is cool" to the end of each name
+for student in students:
+    print(student + " is cool")
+    #5f if the students name is your name add "IT ME!!" to the end
+for student in students:
+    if student == "rokhaya":
+        print(student + "IT ME!!")
+    #5g if you are not in the list print "I guess I'm not cool"
+for student in students:
+    if student != "rokhaya":
+        print("I guess I'm not cool")
+
+    #5h list some methods to remove things from a list
+print(students.pop(int((student[-1]))))  #need to the int() to converted to an int
+
+
+#6 Create a dictionary with keys "Digital Crafts" ,"Instructor", "TA" and values "Bootcamp" ,"Joe" ,"Ethan"
+School = {"Digital Crafts": "Bootcamp" ,
+          "Instructor": "Joe",
+          "TA": "Ethan"}
+ 
+    #6a return the value of "Digital Crafts"
+print(School.values())
+
+
+#7 Write a class named Cars with attributes make,model,year, and type(sedan,truck,crossover, sportscar, etc....)
+class Car:
+    def __init__(self, make, model, year, type):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.type = type
+    
+    #7a Instantiate 3 new Cars
+mercedes = Car("Mercedes","x6", 2021, "suv")
+acura = Car("Acura","rdx", 2022, "suv" )
+jeep = Car("Jeep", "rubicon", 2022, "truck")  
+
+    #7b Add a method that allows you to see the make and model of a car in your terminal
+cars = []
+
+#def viewMakeAndModel():
+      #self.getattr("make","model")
+
+    #7c Use the above method on the second car
+acura.viewMakeAndModel()
+    #7d Add a method that is called "honkHorn" that prints "Beep Beep"
+
+def honkHorn():
+    print("Beep Beep")
+
+
+#Push this all to a repo on your github
+#Paste github link of assignment page to: https://digitalcrafts.instructure.com/courses/225/assignments/9345
